c5d4c05971304c75e1a7481985f9be0964177e5d
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-03-25 01:10:46 +0300

    wow i have a huge error somewhere that i don't know how to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 
 const Pet = (props) => {
   return React.createElement("div", {}, [
@@ -27,5 +27,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
